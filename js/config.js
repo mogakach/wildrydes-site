3ad28ab326ca49fd5229c531a87fbe42bdce7096
mogakach <143662805+mogakach@users.noreply.github.com>
2024-12-26 10:25:06 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-2_WHKfPdpn6', // e.g. us-east-2_uXboG5pAb 
+        userPoolClientId: '7bnsq7lrrr1tcaduv43582fkkd', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv 
+        region: 'us-west-2' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
